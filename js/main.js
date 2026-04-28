b630017087c536a906aa3b9db913a678ef73ec58
ParthNanda27 <nandaparth577@gmail.com>
2026-04-28 15:18:17 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,12 @@ async function loadProjects() {
     const div = document.createElement("div");
     div.className = "project-card";
 
-    div.innerHTML = `
-      <h3>${p.title}</h3>
-      <p><strong>${p.org}</strong></p>
-      <p>${p.desc}</p>
-    `;
+      div.innerHTML = `
+        <h3>${p.title}</h3>
+        <p><strong>${p.org}</strong></p>
+        <p>${p.desc}</p>
+        <a href="project.html?id=${p.id}">View Details →</a>
+      `;
 
     container.appendChild(div);
   });
